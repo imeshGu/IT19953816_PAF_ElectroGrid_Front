88e6b1cb2c0b5d930f6253cdd3af5fe5b01d2497
ImeshGu <imeshguna3@gmail.com>
2022-05-15 13:11:53 +0530

    server model edit

---
+++ Meterprofile/src/main/webapp/Components/meterprofile.js
@@ -0,0 +1,9 @@
+function readMeterprofile(){
+	output += "<tr><td><input id='hidItemIDUpdate' name='hidItemIDUpdate' 
+ 	type='hidden' value='" + itemID + "'>" + itemCode + "</td>";
+}
+
+function readMeterprofile(){
+	output += "<td><input name='btnUpdate' type='button' 
+ 	value='Update' class='btnUpdate btn btn-secondary'></td>"
+}
